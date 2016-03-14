bd09f64f11aab34402db177444a66a71f5db0520
Jason Brown <browniefed@gmail.com>
2016-03-13 18:54:30 -0700

    Call servers every 5 seconds

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var request = require('request-promise');
+var config = require('./config');
+var io = require('socket.io')(http);
+var _ = require('lodash');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var VEHICLE_URL = 'http://developer.trimet.org/ws/v2/vehicles';
+
+var vehicles = [];
+
+io.set('origins', '*:*');
+
+function getVehicles() {
+  request(VEHICLE_URL, {
+    qs: {
+      'appID': config.APPID
+    }
+  }).then(function(data) {
+    var json = JSON.parse(data);
+    if (json && json.resultSet) {
+      vehicles = _.map(json.resultSet.vehicle, function(vehicle) {
+        return _.pick(vehicle, ['routeNumber', 'delay', 'inCongestion', 'latitude', 'longitude', 'type', 'vehicleID']);
+      });
+
+      io.emit('vehicles_update', vehicles);
+    }
+  });
+}
+
+setInterval(getVehicles, 5000)
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function(socket){
+
+  socket.emit('vehicles_update', vehicles);
+
+});
+
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
 });
