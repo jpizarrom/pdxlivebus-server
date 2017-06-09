40e0506382e7598e5e4e28bfef4425bd1981556a
jpizarrom <jpizarrom@gmail.com>
2017-06-09 01:15:13 -0400

    use sqs (#2)

--- server.js
+++ server.js
@@ -6,50 +6,119 @@ var _ = require('lodash');
 
 var cors = require('cors');
 
+var AWS = require('aws-sdk');
+var util = require('util');
+
+// configure AWS
+AWS.config.update({
+  'region': 'us-east-1'
+});
+
+var sqs = new AWS.SQS();
+
+var receiveMessageParams = {
+  QueueUrl: process.env.QUEUE_URL,
+  MaxNumberOfMessages: 1
+};
+
 app.use(cors());
 
 var config = {
   VEHICLE_URL: process.env.VEHICLE_URL,
-  USERNAME: process.env.VEHICLE_URL_USERNAME,
-  PASSWORD: process.env.VEHICLE_URL_PASSWORD,
   LOG_KEY: process.env.VEHICLE_LOG_KEY,
 }
 
+var objKey;
 var vehicles = [];
 
 io.set('origins', '*:*');
 
 function getVehicles() {
-  request(config.VEHICLE_URL, {
-  auth: {
-    user: config.USERNAME,
-    pass: config.PASSWORD,
-    sendImmediately: false
-  }
-  }).then(function(data) {
-    var json = JSON.parse(data);
-    console.log(json[config.LOG_KEY]);
-    if (json && json.posiciones) {
-      vehicles = _.map(json.posiciones, function(vehicle) {
-        var arr = /^([^;]+[;]){0}([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);/.exec(vehicle);
-        return {
-          routeNumber: arr[9],
-          delay: 0,
-          inCongestion: null,
-          latitude: arr[4],
-          longitude: arr[5],
-          type: "bus",
-          vehicleID: arr[3]
-        }
-      });
+  if (objKey){
+    var url = config.VEHICLE_URL+objKey;
+    console.log(url);
+    request(url).then(function(data) {
+      var json = JSON.parse(data);
+      console.log(json[config.LOG_KEY]);
+      if (json && json.posiciones) {
+        vehicles = _.map(json.posiciones, function(vehicle) {
+          var arr = /^([^;]+[;]){0}([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);([^;]+);/.exec(vehicle);
+          return {
+            routeNumber: arr[9],
+            delay: 0,
+            inCongestion: null,
+            latitude: arr[4],
+            longitude: arr[5],
+            type: "bus",
+            vehicleID: arr[3],
+            dateDB:arr[13],
+            dateTrans:arr[2]
+          }
+        });
 
-      io.emit('vehicles_update', vehicles);
-    }
-  });
+        io.emit('vehicles_update', vehicles);
+      }
+    });}
 }
 
-getVehicles();
+// getVehicles();
 setInterval(getVehicles, 60000);
+
+function getMessages() {
+  sqs.receiveMessage(receiveMessageParams, receiveMessageCallback);
+}
+
+function doSomethingCool(body, message){
+  // console.log(body);
+  // console.log(message);
+  event = JSON.parse(body.Message);
+  // console.log('Received event: ', JSON.stringify(event, null, 2));
+  event.Records.forEach(function(record) {
+    var bucket = record.s3.bucket.name;
+    objKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '));
+    console.log(bucket, objKey);
+  });
+
+}
+
+function receiveMessageCallback(err, data) {
+  //console.log(data);
+  if(err) console.log(err);
+
+  if (data && data.Messages && data.Messages.length > 0) {
+
+    for (var i=0; i < data.Messages.length; i++) {
+      var message = data.Messages[i],
+          body = JSON.parse(message.Body);
+      // process.stdout.write(".");
+      //console.log("do something with the message here...");
+      doSomethingCool(body, message);
+      //
+      // Delete the message when we've successfully processed it
+      var deleteMessageParams = {
+        QueueUrl: process.env.QUEUE_URL,
+        ReceiptHandle: data.Messages[i].ReceiptHandle
+      };
+
+      sqs.deleteMessage(deleteMessageParams, deleteMessageCallback);
+    }
+
+    getMessages();
+
+  } else {
+    // process.stdout.write("-");
+    setTimeout(getMessages, 30000);
+  }
+}
+
+function deleteMessageCallback(err, data) {
+  if(err) console.log(err);
+  //console.log("deleted message");
+  //console.log(data);
+}
+
+// getMessages();
+setTimeout(getMessages, 1000);
 
 app.get('/', function(req, res){
   res.send(vehicles);
